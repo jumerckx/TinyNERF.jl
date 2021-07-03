1078b43e9e6b14795cce9690603fb033ba268e3c
merckxiaan <julesmerckx12@gmail.com>
2021-07-03 22:17:59 +0200

    sampling centered pixels

--- src/test.jl
+++ src/test.jl
@@ -1,4 +1,5 @@
-using TinyNERF, Images, Flux, CUDA, EllipsisNotation, Statistics
+using TinyNERF, Images, Flux, CUDA, EllipsisNotation, Statistics, StatsBase
+using Flux: @nograd, Zygote.ignore, params, glorot_uniform
 
 dataset = get_data()
 images = permutedims(dataset["images"], (4, 2, 3, 1)) # Images×Height×Width×Channels -> Channels×Height×Width×Images
@@ -13,13 +14,13 @@ colorview(RGB, testimg)
 images, poses = images[:, :, :, 1:101], poses[:, :, 1:101]
 
 function posenc(x, L_embed=6)
-    out = similar(x, (3 + 3 * 2 * L_embed, size(x, 2)))
+    out = similar(x, (3 + 3 * 2 * L_embed, size(x)[2:end]...))
     out[1:3, ..] .= x
     out[4:end, ..] .= vcat((f.(2^i .* x) for i in 0:(L_embed - 1) for f in (sin, cos))...)
 
     return out
 end
-Flux.@nograd posenc
+@nograd posenc
 
 function init_model(W=256; L_embed=6)
     inputsize = 3 * (1 + 2 * L_embed)
@@ -37,29 +38,48 @@ function init_model(W=256; L_embed=6)
         Dense(W, 4, identity))
 end
 
-function get_rays(H, W, focal, cam2world)
-    dirs = fill(eltype(cam2world)(-1), (3, H, W))
-    dirs[1, :, :] .= (((0:(W - 1)) .- W / 2) ./ focal)'
-    dirs[2, :, :] .= -((0:(H - 1)) .- H / 2) ./ focal
+"""
+    get_rays(pixels, H, W, focal, cam2world)
 
-    dirs = cam2world[1:3, 1:3] * reshape(dirs, (3, H * W))
-    dirs = reshape(dirs, (3, H, W))
+Get origin and directions of rays through `pixels`.
+`pixels` should be a vector of CartesianIndices.
 
-    origin = cam2world[1:3, 4]
+"""
+function get_rays(pixels, H, W, focal, cam2world)
+    origin = similar(cam2world, 3)
+    directions = similar(cam2world, (3, length(pixels)))
 
-    return gpu(origin), gpu(dirs)
+    get_rays!(origin, directions, pixels, H, W, focal, cam2world)
+
+    return origin, directions
 end
-Flux.@nograd get_rays
+
+function get_rays!(origin, directions, pixels, H, W, focal, cam2world)
+    directions[1, :] .= (getindex.(pixels, 2) .- W/2 .- 1/2) ./ focal
+    directions[2, :] .= -(getindex.(pixels, 1) .- H/2 .- 1/2) ./ focal
+    directions[3, :] .= -1
+    directions .= cam2world[1:3, 1:3] * directions
+
+    origin .= cam2world[1:3, 4]
+
+    return origin, directions
+end
+
+gaussian_2d(x,y, σx, σy) = 1/(2π*σx*σy)*exp(-(((x-W/2)/σx)^2+((y-H/2)/σy)^2) / 2)
 
 # using GLMakie
 # begin
 #     s = Scene()
 #     cam3d!(s)
 #     for p in eachslice(poses, dims=3)
-#         origin, dirs = get_rays(H, W, focal, p)
+#         weights = ProbabilityWeights(map((x)->gaussian_2d(x..., W/5, H/5), Tuple.(CartesianIndices((H, W))))[:])
+#         pixels = CartesianIndices((H, W))[sample(1:H*W, weights, 10, replace=false)]
+#         origin, directions = get_rays(pixels, H, W, focal, p)
+#         # origin, directions = get_rays(CartesianIndices((H, W))[:], H, W, focal, p)
+#         # origin, dirs = get_rays(H, W, focal, p)
 #         arrows!(s,
-#             [Point3(origin) for _ in 1:H÷20 for _ in 1:W÷20],
-#             [Vec3(dirs[:, i*20, j*20]) for i in 1:H÷20 for j in 1:W÷20],
+#             [Point3(origin) for _ in 1:size(directions, 2)],
+#             [Vec3(directions[:, i]) for i in 1:size(directions, 2)],
 #             linewidth=0.01,
 #             arrowsize=0,
 #             lengthscale=0.5)
@@ -67,83 +87,76 @@ Flux.@nograd get_rays
 #     display(s)
 # end
 
-function get_z_vals(origin, dirs, near, far, N_samples, randomized=false)
-    H, W = size(dirs)[[2, 3]]
-    z_vals = reshape(range(near, stop=far, length=N_samples), (1, 1, 1, N_samples))
+function get_pts(origin, directions, near, far, n_samples; randomized=false)
+    z_vals = reshape(range(near, stop=far, length=n_samples), 1, 1, n_samples)
     if randomized
-        z_vals = rand(1, H, W, N_samples) .* ((far - near) / N_samples) .+ z_vals
+        z_vals = rand(1, size(directions, 2), n_samples) .* ((far - near) / n_samples) .+ z_vals
     end
-
-    return collect(z_vals)
+    return origin .+ (directions .* z_vals), collect(z_vals)
 end
-Flux.@nograd get_z_vals
 
-function render_rays(emb_fn, network_fn, origin, dirs, near, far, N_samples, randomized=false)
-    z_vals = gpu(get_z_vals(origin, dirs, near, far, N_samples, randomized))
-    pts = origin .+ (dirs .* z_vals)
-    raw = network_fn(emb_fn(reshape(pts, (3, :))))
+function render(emb_fn, network_fn, pts, z_vals)
+    raw = network_fn(emb_fn(pts))
 
-    σₐ = reshape(relu.(raw[end, :]), (H, W, N_samples))
-    rgb = reshape(sigmoid.(raw[1:3, :]), (3, H, W, N_samples))
+    σₐ = relu.(raw[4:4, :, :])
+    rgb = sigmoid.(raw[1:3, :, :])
 
-    e = size(z_vals)[end] # 'end', necessary because of bug in EllipsisNotation: https://github.com/ChrisRackauckas/EllipsisNotation.jl/issues/19
-
-    δ = (z_vals[.., 2:e] .- z_vals[.., 1:e - 1])[1, ..]
-    δ = cat(δ, fill(1e10, size(δ)[[1, 2]]), dims=3)
-
+    δ = cat(z_vals[:, :, 2:end] .- z_vals[:, :, 1:(end-1)], fill(1e10, size(z_vals)[[1, 2]]), dims=3)
     α = 1 .- exp.(-σₐ .* δ)
-    weights = α .* cumprod(cat(gpu(ones(H, W)), (1 .- α .+ 1e-10)[:, :, 1:end - 1], dims=3), dims=3) # exclusive cumprod
-    weights = reshape(weights, 1, size(weights)...)
+    weights = α .* cat(gpu(ones(1, size(α, 2))), cumprod((1 .- α .+ 1e-10)[:, :, 1:end-1], dims=3), dims=3) # exclusive cumprod
     
-    rgb_map = sum(rgb .* weights, dims=4)[.., 1]
-    depth_map = sum(z_vals .* weights, dims=4)[1, .., 1]
-    acc_map = sum(weights, dims=4)[1, .., 1]
-
-    return rgb_map, depth_map, acc_map
+    rgb_map = sum(rgb .* weights, dims=3)[:, :, 1]
 end
 
-# rgb, depth, acc = render_rays(posenc, gpu(init_model()), get_rays(H, W, focal, testpose)..., 2, 6, 64, true)
+function train!(model, opt, images, poses; n_samples=64, batch_size=3000, n_iters=1000, i_plot=25)
+    @assert batch_size <= H*W
 
-function train!(emb_fn, network_fn, images, poses, opt, N_samples, N_iters; i_plot=25)
-    PSNRs = []
-    iternums = []
+    PSNRs, iternums = [], []
     
-    local training_loss
     ps = params(model)
-    for i in 1:N_iters
-        println("iteration $i")
-        img_i = rand(1:size(images, 4))
-        target = gpu(images[:, :, :, img_i])
-        pose = poses[:, :, img_i]
-        
-        rays_o, rays_d = get_rays(H, W, focal, pose)
+    local ℒ
+    
+    for i in 1:n_iters
+        img_index = rand(1:size(images, 3))
+        pose = poses[:, :, img_index]
+
+        weights = ProbabilityWeights(map((x)->gaussian_2d(x..., W/5, H/5), Tuple.(CartesianIndices((H, W))))[:])
+
+        pixels = CartesianIndices((H, W))[sample(1:H*W, weights, batch_size, replace=false)]
+
+        target = gpu(images[:, :, :, img_index][:, pixels])
+
+        origin, directions = get_rays(pixels, H, W, focal, pose)
+        pts, z_vals = gpu(get_pts(origin, directions, 2, 5, n_samples, randomized=true))
         
         gs = gradient(ps) do
-            rgb, depth, acc = render_rays(emb_fn, network_fn, rays_o, rays_d, 2, 5, N_samples, true)
-            training_loss = mean((rgb .- target).^2)
-            return training_loss
+            prediction = render(posenc, model, pts, z_vals)
+            ℒ = mean((prediction .- target).^2)
+            return ℒ
         end
+
         Flux.update!(opt, ps, gs)
-        if i%i_plot==0
-            rays_o, rays_d = get_rays(H, W, focal, testpose)
-            rgb, depth, acc = render_rays(posenc, model, rays_o, rays_d, 2, 6, N_samples)
-            rgb = cpu(rgb)
+        println(i)
+        if i%i_plot == 0
+            pixels = CartesianIndices((H, W))[:]
+            origin, directions = get_rays(pixels, H, W, focal, testpose)
+            pts, z_vals = gpu(get_pts(origin, directions, 2, 5, n_samples))
+
+            prediction = render(posenc, model, pts, z_vals)
+            rgb = reshape(cpu(prediction), (3, H, W))
             display(colorview(RGB, rgb))
             
             testing_loss = mean((rgb .- testimg).^2)
             PSNR = -10 * log10(testing_loss)
-            println("training loss: $training_loss\ttesting loss: $testing_loss\tPSNR: $PSNR")
+            println("training loss: $ℒ\ttesting loss: $testing_loss\tPSNR: $PSNR")
             push!(PSNRs, PSNR)
             push!(iternums, i)
         end
     end
-    return iternums, PSNRs
+    return PSNRs, iternums
 end
 
-model = gpu(init_model())
-optimizer = ADAM(5e-4)
+opt = ADAM(5e-4)
+m = gpu(init_model())
 
-N_samples = 10
-N_iters = 1000
-
-train!(posenc, model, images, poses, optimizer, N_samples, N_iters)
+train!(m, opt, images, poses, n_samples=64, batch_size=500, n_iters=1000, i_plot=25)
